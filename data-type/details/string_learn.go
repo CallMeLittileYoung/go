dd76ab7486775b2aeb3b9b44753454366a8b3fb9
young <1249356035@qq.com>
2022-06-07 19:49:31 +0800

    string learn

--- data-type/details/string_learn.go
+++ data-type/details/string_learn.go
@@ -8,4 +8,20 @@ func stringLearn() {
 	str2 := "叫我小年轻"
 	fmt.Printf("The Length of %s is %d \n", str, len(str))
 	fmt.Printf("str2 is %s \n", str2)
+	//多行字符串
+	results := `Search results for "Golang":
+				- Go
+				- Golang
+                Golang Programming
+                `
+	fmt.Printf("%s", results)
+
+	str4 := "叫我小年轻"
+	n := len(str4)
+	fmt.Println(n)
+	for i, ch := range str4 {
+		fmt.Println(i, string(ch))
+	}
+	str5 := str4[:3] //叫
+	fmt.Println(str5)
 }
