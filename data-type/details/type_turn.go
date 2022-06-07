2e2e6c0b066cafd00da60569b84f415f912108ca
young <1249356035@qq.com>
2022-06-07 20:10:50 +0800

    类型转换

--- data-type/details/type_turn.go
+++ data-type/details/type_turn.go
@@ -1,6 +1,9 @@
 package details
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func turn() {
 	v1 := uint8(16)
@@ -8,4 +11,14 @@ func turn() {
 
 	v2 := uint16(255)
 	fmt.Println(v2)
+
+	v3 := 99.99
+	v4 := int(v3)
+	fmt.Println(v4)
+
+	v5 := "100"
+	v6, err := strconv.Atoi(v5)
+	if err == nil {
+		fmt.Println(v6)
+	}
 }
