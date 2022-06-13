b591b8e07c5749a2bd704dc3b2edb628c3809bc4
young <1249356035@qq.com>
2022-06-13 17:09:05 +0800

    go的接口定义与实现

---
+++ interfaces/File.go
@@ -0,0 +1,17 @@
+package interfaces
+
+type File struct {
+}
+
+func (f *File) Read(buf []byte) (n int, err error) {
+	return 1, nil
+}
+func (f *File) Write(buf []byte) (n int, err error) {
+	return 1, nil
+}
+func (f *File) Seek(off int64, whence int) (pos int64, err error) {
+	return 1, nil
+}
+func (f *File) Close() error {
+	return nil
+}
