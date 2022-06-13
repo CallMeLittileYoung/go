dfe0c4b4306332e42acab85e44de0b75b58ccb5a
young <1249356035@qq.com>
2022-06-13 19:00:45 +0800

    <-ch接受通道信息(等待协程结束)

---
+++ guc/channel_learn.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func chanLearn() {
+
+	s := time.Now()
+	chs := make([]chan int, 100)
+
+	for i := 0; i < 100; i++ {
+		chs[i] = make(chan int)
+		go Add3(i, 1, chs[i])
+	}
+
+	for _, ch := range chs {
+		//fmt.Println(ch)
+		<-ch
+		//我们再通过 <-ch 语句从通道切片 chs 中依次接收数据（不对结果做任何处理，相当于写入通道的数据只是个标识而已，表示这个通道所属的协程逻辑执行完毕）
+	}
+	fmt.Println("cost", time.Since(s))
+}
+
+func Add3(a, b int, ch chan int) {
+
+	fmt.Println("a ", a, " + b ", b, " sum is ", a+b)
+	ch <- 1
+}
