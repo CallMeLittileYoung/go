0053ec4775ce3b1baf263ef9f6e82e28ea7ef2ab
liyuedong <liyuedong@joyy.com>
2022-07-11 11:30:24 +0800

    path的包含性

--- go-program-design/chapter1/web.go
+++ go-program-design/chapter1/web.go
@@ -4,18 +4,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
 
+//生命两个常量
+var mu sync.Mutex
+var count int
+
 func InitWeb() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler) //所有请求都会走入该方法
 	http.HandleFunc("/gif", gif2)
+	http.HandleFunc("/counter", counter)
 	log.Fatal(http.ListenAndServe("127.0.0.1:10086", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	fmt.Println("Again")
+	mu.Unlock()
 	fmt.Fprintf(w, "URL.path is %q \n", r.URL.Path)
 }
 
 func gif2(w http.ResponseWriter, r *http.Request) {
 	Lissajous(w)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "counter is %d ", count)
+	mu.Unlock()
+
 }
