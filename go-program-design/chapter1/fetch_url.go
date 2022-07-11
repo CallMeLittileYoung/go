e4bf760ae5c24a164fad6c33d65d67d974c2d4b6
liyuedong <liyuedong@joyy.com>
2022-07-11 10:14:02 +0800

    使用io.copy 拷贝

--- go-program-design/chapter1/fetch_url.go
+++ go-program-design/chapter1/fetch_url.go
@@ -2,7 +2,7 @@ package chapter1
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 )
@@ -12,14 +12,25 @@ func FetchUrl() {
 	var s string = "https://www.google.com.hk/?hl=zh-cn"
 	get, err := http.Get(s)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err, err)
 		os.Exit(-1)
 	}
-	all, err := ioutil.ReadAll(get.Body)
-	get.Body.Close()
+	code := get.StatusCode
+	fmt.Printf("code is %v \n", code)
+
+	stdout := os.Stdout
+	//io.Copy(stdout)
+	// io.copy 新函数
+	_, err = io.Copy(stdout, get.Body)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", s, err)
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err, err)
 		os.Exit(-1)
 	}
-	fmt.Printf("%s", all)
+	//all, err := ioutil.ReadAll(get.Body)
+	//get.Body.Close()
+	//if err != nil {
+	//	fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", s, err)
+	//	os.Exit(-1)
+	//}
+	//fmt.Printf("%s", all)
 }
