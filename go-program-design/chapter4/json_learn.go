64b27f648acd4c9f261762c96273298c4c959401
liyuedong <liyuedong@joyy.com>
2022-07-22 18:29:04 +0800

    json

---
+++ go-program-design/chapter4/json_learn.go
@@ -0,0 +1,7 @@
+package chapter4
+
+type Movie struct {
+	Title    string
+	Released string
+	Actors   []string
+}
---
+++ go-program-design/chapter4/test/json_learn_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"encoding/json"
+	"fmt"
+	"go-program-design/chapter4"
+	"testing"
+)
+
+func Test_Json(t *testing.T) {
+
+	movie := chapter4.Movie{
+		Title:    "aa",
+		Released: "bb",
+		Actors:   []string{"aa", "b"},
+	}
+	indent, _ := json.MarshalIndent(movie, "", " ")
+	fmt.Printf("%s\n", indent)
+}
